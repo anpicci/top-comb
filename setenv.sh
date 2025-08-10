28bcba5be75a6d2f935fcfb55a8bc497ec42ab72
Carlos Vico Villalba <carlos.vico.villalba@cern.ch>
2025-08-10 17:03:50 +0200

    Staged inputs to the combination in a folder within this repo

--- setenv.sh
+++ setenv.sh
@@ -6,6 +6,6 @@ export TOPCOMB_OUTPATH=/eos/cms/store/group/phys_top/cvicovil/
 
 # Other
 export TOPCOMB_MAINPATH=`pwd`
-export TOPCOMB_ANALYSES=`realpath topcomb_analyses`
+export TOPCOMB_ANALYSES=`realpath inputs`
 export TOPCOMB_GENPRODUCTIONS=`realpath genproductions_scripts`
 export TOPCOMB_CMGRDF=`realpath cmgrdf-prototype`
