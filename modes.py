9baaf58208f93943d90bc802d3ed733ee89f4336
Carlos Vico Villalba <carlos.vico.villalba@cern.ch>
2026-01-21 22:08:19 +0100

    New mode for getting inputs to the combination code

--- modes.py
+++ modes.py
@@ -4,19 +4,22 @@ Each builder is a callable that returns a function accepting an environment dict
 """
 import os
 import subprocess
-from utils import get_logger
-from utils import load_config
+from utils import (
+    get_logger,
+    load_config,
+    create_dir
+
+)
 logger = get_logger(__name__)
 
-def _setup_gen():
+def _setup():
     """Builder for setting up GEN related aspects."""
     from pathlib import Path
     from gen_tools import (
-        GenerationConfig,
-        _prepare_fragment,
-        _prepare_gridpack,
-        _setup_madgraph,
-        _prepare_nanogen
+        madgraph_utils,
+        gridpack_utils,
+        fragment_utils,
+        nanogen_utils,
     )
     from utils import (
         load_config, 
@@ -44,25 +47,80 @@ def _setup_gen():
         genoutpath = os.path.join(
             environment.get('outpath'),
             environment.get("tag"),
+            "gridpacks",
         )
         
-        config = GenerationConfig(
-            measurement_name = measurement_name,
-            workdir = Path( environment.get("workdir") ),
-            outpath = f"{environment.get('eos_redirector')}{genoutpath}",
-            mcprod_path = environment.get("mcprod"),
-            genprod_image = environment.get("genproductions_image"),
-            genprod_repo = environment.get("genproductions_repo"),
-            genprod_branch = environment.get("genproductions_branch")
+        # Setup the common part (reweighting maps and whatnot)
+        measurement_dir = Path(environment.get("workdir")) / measurement_name
+        os.makedirs( measurement_dir , exist_ok=True )
+        rwgt_points = madgraph_utils._generate_reweight_points(
+            operators
         )
 
-        # Process each sample
+        # Save a mapping json tied to the measurement phase space
+        madgraph_utils._build_reweight_mapping(
+            measurement_dir,
+            rwgt_points,
+            operators
+        )
+
+        # Save also a README for quick view in gitlab
+        madgraph_utils._build_reweight_readme(
+            measurement_dir,
+            rwgt_points,
+            operators
+        )
+
+        ## Process each sample
         samples = gen_metadata["samples"]
         for proc_metadata in samples:
-            _setup_madgraph( proc_metadata, operators, config )
-            _prepare_fragment( proc_metadata, config)
-            _prepare_gridpack( proc_metadata, config )
-            _prepare_nanogen( proc_metadata, config )
+
+            # Prepare the process directory
+            procname = proc_metadata["name"]
+            restrict_name = proc_metadata["template_restrict_card"]["restrict_name"]
+            procdir = measurement_dir / "mcgen" / proc_metadata["name"]
+            create_dir( procdir )
+            create_dir( procdir / "mgcards" )
+            files = [
+                ( procdir / "mgcards" / f"{procname}_proc_card.dat" , madgraph_utils.prepare_proc_card( proc_metadata ) ),
+                ( procdir / "mgcards" / f"{procname}_run_card.dat" , madgraph_utils.prepare_run_card( proc_metadata ) ),
+                ( procdir / "mgcards" / f"{procname}_extramodels.dat" , madgraph_utils.prepare_extramodels( proc_metadata ) ),
+                ( procdir / "mgcards" / f"{procname}_customizecards.dat" , madgraph_utils.prepare_customizecards( proc_metadata, operators ) ),
+                ( procdir / "mgcards" / f"{procname}_restrict_{restrict_name}.dat" , madgraph_utils.prepare_restrict_card( proc_metadata, operators ) ),
+                ( procdir / "mgcards" / f"{procname}_reweight_card.dat" , madgraph_utils.prepare_reweightcards( rwgt_points ) ),
+
+            ]
+
+            for card_path, card_content in files:
+                card_path.parent.mkdir(parents=True, exist_ok=True)
+                with open( card_path, "w") as card_file:
+                    card_file.write( card_content )
+            
+
+            # Prepare scripts and configurations
+            redirector = environment.get("eos_redirector")
+            gridpack_location = gridpack_utils._prepare_gridpack(
+                measurement_name,
+                proc_metadata,
+                f"{redirector}{genoutpath}",
+                procdir,
+                environment.get("genproductions_image"),
+                environment.get("genproductions_repo"),
+                environment.get("genproductions_branch")
+            ) 
+
+            fragment_path = procdir / "fragment.py" 
+            fragment_content = fragment_utils._prepare_fragment( gridpack_location, proc_metadata ) 
+            with open( fragment_path, "w" ) as fragment_file:
+                fragment_file.write( fragment_content )
+
+            
+            # Finally, prepare the nanogen configuration
+            nanogen_utils._prepare_nanogen(
+                procdir=str(procdir),
+                mcprod_path=environment.get("mcpath"),
+                proc_metadata=proc_metadata,
+            )
     
     return setup_gen_config
 
@@ -74,9 +132,10 @@ def _submit_gen():
         what = environment.get("what") 
         submit = environment.get("submit")
         combdir = environment.get("workdir")
+        measurement_name = environment.get("measurement")
 
         # Check if the processes directory exists
-        processes_path = os.path.join(combdir, "processes")
+        processes_path = os.path.join(combdir, measurement_name, "mcgen")
         print( os.listdir( processes_path ) )
         if not os.path.isdir(processes_path):
             logger.error(f"No 'processes' folder found in {combdir}. Run setup first.")
@@ -138,7 +197,11 @@ def _reinterpret():
         """
 
         # Load the main configurations
-        outpath = environment.get("outpath")
+        reinterpretoutpath = os.path.join(
+            environment.get('outpath'),
+            environment.get("tag"),
+            "shapes",
+        )
         ncores = environment.get("ncores")
         debug = environment.get("debug")
         doUnc = environment.get("doUnc")
@@ -151,7 +214,7 @@ def _reinterpret():
         logger.warning(f"Setting measurement {measurement_name}")
         reinterpret_one_measurement(
             measurement_name = measurement_name,
-            outpath = outpath, 
+            outpath = reinterpretoutpath, 
             metadata = reinterpret_meta,
             lumis = environment.get("lumis"),
             ncores = ncores,
@@ -162,6 +225,39 @@ def _reinterpret():
         logger.info("measurement setup completed.")
 
     return make_reinterpretation
+
+def _cook():
+    """Builder for 'reinterpret' mode."""
+    def cook_inputs( environment ) -> None:
+
+        """
+        Top-level entry to prepare the inputs for the combination,
+        based on the results of the reinterpretation.
+        """
+        main_config = environment.get("main_config")["MEASUREMENTS"]
+        measurement_name = environment.get("measurement")
+        measurement_config = main_config.get( measurement_name )
+        observable = measurement_config.get("observable")
+
+        logger.info(f"Preparing inputs for measurement {measurement_name}, observable {observable}")
+
+        config_path = os.path.join(
+            "measurements",
+            measurement_name,
+            "observable_configs",
+            f"{observable}.yaml"
+        )
+        from utils.combination_chef import CombinationChef
+        chef = CombinationChef(
+            measurement_name=measurement_name,
+            observable=observable,
+            config_path=config_path,
+            environment=environment,
+        )
+
+        
+
+    return cook_inputs 
 
 def _setup_combine():
     """Builder for installing combine. Executes a script inside `combine_tools`."""
@@ -197,13 +293,16 @@ MODE_REGISTRY = {
     # Each entry provides:
     #  - "funcs": list of builders that return a function accepting environment
     "setup": {
-        "funcs": [_setup_gen],
+        "funcs": [_setup],
     },
     "submit": {
         "funcs": [_submit_gen],
     },
     "reinterpret": {
         "funcs": [_reinterpret],
+    },
+    "cook": {
+        "funcs": [_cook],
     },
     "setup_combine": {
         "funcs": [_setup_combine],
